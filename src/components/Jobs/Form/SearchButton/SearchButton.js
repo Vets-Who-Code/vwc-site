ae4cac5175dd5337958c9e5048edb6bd86bf9b7e
Stephan Lamoureux <sa.lamoureux@gmail.com>
2021-07-16 14:07:26 -0400

    condensed form.js into one component, added image and video files, moved nav link

--- src/components/Jobs/Form/SearchButton/SearchButton.js
+++
@@ -1,9 +0,0 @@
-function SearchButton() {
-  return (
-    <button type="submit" className="btn btn-primary mb-2 submit-button search">
-      <i className="glyphicon glyphicon-search">&nbsp;Search</i>
-    </button>
-  )
-}
-
-export default SearchButton
